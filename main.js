73580d3fab9cc4a9d9ef73ad3068e0494eb6229c
Rorchik <hotaboriginal@gmail.com>
2022-03-09 21:17:50 +0300

    addjs

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-let userName = ['Марк', 'Давид', 'Жорик'];
-userName.sort();
-console.log(`Привет! Сегодня ${userName}`);
+for (var i in ['input', 'change', 'blur', 'keyup']) {
+    cardcode.addEventListener('input', formatCardCode, false);
+}
+function formatCardCode() {
+    var cardCode = this.value.replace(/[^\d]/g, '').substring(0,16);
+    cardCode = cardCode != '' ? cardCode.match(/.{1,4}/g).join(' ') : '';
+    this.value = cardCode;
+    myform.number.value=this.value.split(" ").join("");
+}
