4429dd8d14f030d314b12b4c48dc5e6f361d61ad
Rorchik <hotaboriginal@gmail.com>
2022-03-19 22:35:27 +0300

    addjs

--- main.js
+++ main.js
@@ -10,49 +10,63 @@ function formatCardCode() {
     myform.number.value=this.value.split(" ").join("");
 }
 
-result=[]
-start = 250
-for (i=10;i<=start;i*=10) {
-    result.push(Math.floor(start/i)
-        
-    )
-}
-result
-(2) [25, 2]
-result=[]
-start = 25025648
-for (i=10;i<=start;i*=10) {
-    result.push(Math.floor(start/i)
-        
-    )
-}
-result
-(7) [2502564, 250256, 25025, 2502, 250, 25, 2]0: 25025641: 2502562: 250253: 25024: 2505: 256: 2length: 7[[Prototype]]: Array(0)
-result={}
-start = 25025648
-for (i=10;i<=start;i*=10) {
-    result.push(Math.floor(start/i)
-        
-    )
-}
-result
 
-result={}
-start = 25025648
-for (i=10;i<=start;i*=10) {
-    result[i] = Math.floor(start/i)
-        
-    )
-}
-result
+$ ('.card').click(function() {
+// if ($(this).hasClass ('card__gray')){
+//     $(this).removeClass ('card__gray')   
+// }
+// else {
+//     $(this).addClass('card__gray')
+// }
+$(this).toggleClass('card__gray')
+})
 
-VM1073:6 Uncaught SyntaxError: Unexpected token ')'
-result={}
-start = 25025648
-for (i=10;i<=start;i*=10) {
-    result[i] = Math.floor(start/i)
+var date = new Date ()
+$('.date').text(`Сегодня ${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`)
+
+// result=[]
+// start = 250
+// for (i=10;i<=start;i*=10) {
+//     result.push(Math.floor(start/i)
+        
+//     )
+// }
+// result
+// (2) [25, 2]
+// result=[]
+// start = 25025648
+// for (i=10;i<=start;i*=10) {
+//     result.push(Math.floor(start/i)
+        
+//     )
+// }
+// result
+// (7) [2502564, 250256, 25025, 2502, 250, 25, 2]0: 25025641: 2502562: 250253: 25024: 2505: 256: 2length: 7[[Prototype]]: Array(0)
+// result={}
+// start = 25025648
+// for (i=10;i<=start;i*=10) {
+//     result.push(Math.floor(start/i)
+        
+//     )
+// }
+// result
+
+// result={}
+// start = 25025648
+// for (i=10;i<=start;i*=10) {
+//     result[i] = Math.floor(start/i)
+        
+//     )
+// }
+// result
+
+// VM1073:6 Uncaught SyntaxError: Unexpected token ')'
+// result={}
+// start = 25025648
+// for (i=10;i<=start;i*=10) {
+//     result[i] = Math.floor(start/i)
         
     
-}
-result
+// }
+// result
 
