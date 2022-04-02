1c5d4b2006b718720a769f99ef89487e5503f4b3
rorchick <hotaboriginal@gmail.com>
2022-04-02 14:32:27 +0300

    addvalidate

--- main.js
+++ main.js
@@ -1,56 +1,140 @@
-//Маска номера карты
-for (var i in ['input', 'change', 'blur', 'keyup']) {
-    cardcode.addEventListener('input', formatCardCode, false);
-}
 
-function formatCardCode() {
-    var cardCode = this.value.replace(/[^\d]/g, '').substring(0,16);
-    cardCode = cardCode != '' ? cardCode.match(/.{1,4}/g).join(' ') : '';
-    this.value = cardCode;
-    myform.number.value=this.value.split(" ").join("");
-}
+//Дата время(тест-ок)
+jQuery(function($) {
+    setInterval(function() {
+    var Months = ['январь', 'февраль', 'Мартик', 'Апрель']
+    var date = new Date()  
+    var m = Months[date.getMonth()]
+    time = date.toLocaleTimeString();
+    $(".date").text(`Сейчас ${time}, ${date.getDate()} ${m} ${date.getFullYear()} года`);
+    });
+});
 
-//Маска даты
-for (var i in ['input', 'change', 'blur', 'keyup']) {
-    carddate.addEventListener('input', formatcarddate, false);
-}
-
-function formatcarddate() {
-    var carddate = this.value.replace(/[^\d]/g, '').substring(0,4);
-    carddate = carddate != '' ? carddate.match(/.{1,2}/g).join('/') : '';
-    this.value = carddate;
-    myform.number.value=this.value.split(" ").join("");
-}
-
-//Маска имя пользователя
-for (var i in ['input', 'change', 'blur', 'keyup']) {
-    cardname.addEventListener('input', formatcardname, false);
-}
-
-function formatcardname() {
-    var cardname = this.value.replace(/[^A-Z\s]+/ig, '').substring(0,30);
-    this.value = cardname;
-    myform.number.value=this.value.split(" ").join("");
-}
-
-$('.button_settings, .button_okk').click(function(e) {e.preventDefault()
+//Редактировать карту
+$('.button_okk, .button_settings').click(function(e) {e.preventDefault()
     $('.inputvalue').toggleClass('inputvalue__off')
     $('.button_settings').toggleClass('button_okk')
-    $('.number').text(($('#cardcode').val()))
-    $('.cardholder').text(($('#cardname').val()))
-    $('.validdate').text(($('#carddate').val())) 
+    $('.number__off').toggleClass('number').text(($('#cardcode').val()))
+    $('.cardholder__off').toggleClass('cardholder').text(($('#cardname').val()))
+    $('.validdate__off').toggleClass('validdate').text(($('#carddate').val()))
+})
+
+//Клонирование карты(тест-ок)
+$('#add').click(function(e) {e.preventDefault()
+    $(".card:first").clone(true).appendTo(".wrapper");
+})
+
+// Удаление карты(тест-ок)
+$('#clear').click(function(e) {e.preventDefault()
+    if ($('.card').length >1) {
+        $(".card:last-child").detach();	
+        console.log($('.card').length)
+    } else 
+    alert ('Оставьте одну карту для редактирования')
 })
 
 
-//Применить изменения в резерв
-// $('.button_okk, .number, .validdate, .cardholder').click(function(e) {e.preventDefault()
-//     $('.inputvalue').toggleClass('inputvalue__off')
+//Только заглавные(тест-ок)
+$('#cardname').bind('input', function(){
+	$(this).val($(this).val().toUpperCase());
+});
+
+//Валидация ввода номера карты(тест-ок)
+function digits_int(target){
+    val = $(target).val().replace(/[^\d]/g, '').substring(0,16);
+    val = val != '' ? val.match(/.{1,4}/g).join(' ') : '';
+	$(target).val(val);
+}
+
+$(function($){
+	$('#cardcode').on('keyup keypress blur change', function(e){
+		digits_int(this);
+	});
+	digits_int('#cardcode');
+});
+
+//Валидация срока действия карты(тест-ок)
+function digits_int_date(target){
+    val = $(target).val().replace(/[^\d]/g, '').substring(0,4);
+    val = val != '' ? val.match(/.{1,2}/g).join('/') : '';
+	$(target).val(val);
+}
+
+$(function($){
+	$('#carddate').on('keyup keypress blur change', function(e){
+		digits_int_date(this);
+	});
+	digits_int_date('#carddate');
+});
+
+//Валидация имени и фамилии 
+function digits_int_name(target){
+    val = $(target).val().replace(/[^A-Z\s]+/ig, '').substring(0,30);
+	$(target).val(val);
+}
+
+$(function($){
+	$('#cardname').on('keyup keypress blur change', function(e){
+		digits_int_name(this);
+	});
+	digits_int_name('#cardname');
+});
+
+// //Маска номера карты
+// for (var i in ['input', 'change', 'blur', 'keyup']) {
+//     cardcode.addEventListener('input', formatCardCode, false);
+// }
+
+// function formatCardCode() {
+//     let cardCode = this.value.replace(/[^\d]/g, '').substring(0,16);
+//     cardCode = cardCode != '' ? cardCode.match(/.{1,4}/g).join(' ') : '';
+//     this.value = cardCode;
+//	val = $(target).val().replace(/[^0-9]/g, '');
+//	val = val.replace(/B(?=(d{3})+(?!d))/g, ' ');
+
+//     myform.number.value=this.value.split(" ").join("");
+// }
+
+
+// //Маска срока действия
+// for (var i in ['input', 'change', 'blur', 'keyup']) {
+//     carddate.addEventListener('input', formatcarddate, false);
+// }
+
+// function formatcarddate() {
+//     var carddate = this.value.replace(/[^\d]/g, '').substring(0,4);
+//     carddate = carddate != '' ? carddate.match(/.{1,2}/g).join('/') : '';
+//     this.value = carddate;
+//     myform.number.value=this.value.split(" ").join("");
+// }
+
+// //Маска имя пользователя
+// for (var i in ['input', 'change', 'blur', 'keyup']) {
+//     cardname.addEventListener('input', formatcardname, false);
+// }
+
+// function formatcardname() {
+//     var cardname = this.value.replace(/[^A-Z\s]+/ig, '').substring(0,30);
+//     this.value = cardname;
+//     myform.number.value=this.value.split(" ").join("");
+// }
+
+//Двойной клик по полю номера, в разработке
+// $('.number__off, .number').on('dblclick', function() {
+//     $('#cardcode').toggle()
 //     $('.button_settings').toggleClass('button_okk')
-//     $('.number').text(($('#cardcode').val()))
-//     $('.cardholder').text(($('#cardname').val()))
-//     $('.validdate').text(($('#carddate').val()))
-    
+//     $('.number__off').toggleClass('number')
+//     $('.validdate__off').toggleClass('validdate')
+//     $('.cardholder__off').toggleClass('cardholder')
+//     $('.number__off').text(($('#cardcode').val()))
+//     $('.cardholder__off').text(($('#cardname').val()))
+//     $('.validdate__off').text(($('#carddate').val())) 
 // })
+
+
+
+//Применить изменения в резерв
+
 
 
 //Добавить номер карты
@@ -73,16 +157,6 @@ $('#btn_all_clear').click(function(e){e.preventDefault()
     $('.inputvalue').toggleClass('inputvalue__off').slideToggle()   
 });
 
-//Дата время
-jQuery(function($) {
-    setInterval(function() {
-    var Months = ['январь', 'февраль', 'Мартик']
-    var date = new Date()  
-    var m = Months[date.getMonth()]
-    time = date.toLocaleTimeString();
-    $(".date").text(`Сейчас ${time}, ${date.getDate()} ${m} ${date.getFullYear()} года`);
-    });
-});
 
 
 //Факультатив
@@ -110,14 +184,19 @@ jQuery(function($) {
 //     )
 // }
 // result
+// let sum = 0;
 
-// result={}
-// start = 25025648
-// for (i=10;i<=start;i*=10) {
-//     result[i] = Math.floor(start/i)
-//     )
+// while (true) {
+
+//   let value = +prompt("Введите число", '');
+
+//   if (!value) break; // (*)
+
+//   sum += value;
+
 // }
-// result
+// alert( 'Сумма: ' + sum );
+
 
 // VM1073:6 Uncaught SyntaxError: Unexpected token ')'
 // result={}
@@ -127,4 +206,3 @@ jQuery(function($) {
 
 // }
 // result
-
